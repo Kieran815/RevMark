17d65fa78202000f1743fb1f73ead225d7ca2da2
Kieran815 <kieran.milligan@gmail.com>
2020-07-12 22:25:48 -0500

    added weekly service

--- src/pages/sermons.js
+++ src/pages/sermons.js
@@ -24,6 +24,31 @@ const Sermons = () => (
               title="Sermon 2020/06/28"
               width="560"
               height="315"
+              src="https://www.youtube.com/embed/WFWTVrcOx4g"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <a
+              href="https://revmmm.files.wordpress.com/2020/07/july-12-bulletin.pdf"
+              className="button"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                marginBottom: '0.5em',
+                display: 'flex',
+                justifyContent: 'center'
+              }}
+            >
+              Bulletin: Sunday, July 12th
+            </a>
+          </section>
+          <section>
+            <iframe
+              className="yt_videos"
+              title="Sermon 2020/06/28"
+              width="560"
+              height="315"
               src="https://www.youtube.com/embed/Ia_Mh1bE608"
               frameBorder="0"
               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
